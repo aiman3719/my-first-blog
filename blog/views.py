f4381b22d3411fa6a4116784bd1682611c2fbd3d
aiman3719 <aiman3719@gmail.com>
2018-07-11 22:20:08 +0900

    댓글작성하기 기능추가

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from .models import Post
-from .forms import PostForm
+from .forms import CommentForm, PostForm
 
 # Create your views here.
 def post_list(request):
@@ -85,3 +85,21 @@ def post_publish(request, pk):
     post.publish()
 
     return redirect('post_detail', pk=pk)
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/add_comment_to_post.html', {
+        'form': form
+    })
