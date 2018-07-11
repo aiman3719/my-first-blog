f89b5566484a1ae3d5f600903a5f1b44be9e094c
aiman3719 <aiman3719@gmail.com>
2018-07-11 14:28:51 +0900

    mysite/urls.py에 root url추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url, include
+from django.conf.urls import redirect, url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
 ]
